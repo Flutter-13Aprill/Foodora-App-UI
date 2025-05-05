a891265f10b0bde4cab64fddf809db21622cc5a9
BushraAljuwair <bushraaljuwair@gmail.com>
2025-05-05 21:32:36 +0300

    change in main

--- foodor_aapp/lib/main.dart
+++ foodor_aapp/lib/main.dart
@@ -1,12 +1,8 @@
  
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:foodor_aapp/Screens/dashboard_screen.dart';
- import 'package:foodor_aapp/Screens/login_screen.dart';
-import 'package:foodor_aapp/Screens/signup_screen.dart';
-import 'package:foodor_aapp/Screens/test.dart';
 import 'package:foodor_aapp/Screens/welcome_screen.dart';
-import 'package:foodor_aapp/widgets/food_categories.dart';
+ 
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +21,7 @@ Widget build(BuildContext context) {
       return MaterialApp(
         debugShowCheckedModeBanner: false,
         
-        home:  DashboardScreen(), // Use const for performance
+        home:  WelcomeScreen(), // Use const for performance
       ); // MaterialApp
     },
   );
